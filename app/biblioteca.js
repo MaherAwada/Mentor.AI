99953d9b801f0f685c414e83081f512c9031c38d
Maher Ahmad Awada <maherawada2@gmail.com>
2025-11-19 16:16:57 -0300

    fix: ajuste caminho Biblioteca

--- app/biblioteca.js
+++ app/biblioteca.js
@@ -1,4 +1,4 @@
-import BibliotecaScreen from "../src/screens/bibliotecaScreen";
+import BibliotecaScreen from "../src/screens/BibliotecaScreens";
 
 export default function Page() {
   return <BibliotecaScreen />;
